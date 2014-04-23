6ccc544a50ecbf5fa0de351e8c6b068433a49460
Matthew Spellings <mspells@umich.edu>
2014-04-23 11:11:03 -0400

    Move template bits into header

--- src/GTAR.cpp
+++ src/GTAR.cpp
@@ -8,33 +8,12 @@
 #include <stdexcept>
 #include <stdint.h>
 
-#define IS_BIG_ENDIAN (*(uint16_t *)"\0\xff" < 0x100)
-
 namespace gtar{
 
     using std::runtime_error;
     using std::string;
     using std::swap;
     using std::vector;
-
-    // Swap the bytes of a series of characters if this is a big-endian machine
-    template<typename T>
-    void maybeSwapEndian(T *target, size_t byteLength)
-    {
-        char *recast((char*) target);
-
-        if(byteLength % sizeof(T) != 0)
-            throw runtime_error("Trying to convert an incorrect number of bytes to little-endian");
-
-        if(IS_BIG_ENDIAN)
-        {
-            for(size_t i(0); i < byteLength/sizeof(T); ++i)
-            {
-                for(size_t j(0); j < sizeof(T)/2; ++j)
-                    swap(recast[i*sizeof(T) + j], recast[(i + 1)*sizeof(T) - j - 1]);
-            }
-        }
-    }
 
     void GTAR::writeString(const string &path, const string &contents)
     {
@@ -49,42 +28,6 @@ namespace gtar{
     void GTAR::writePtr(const string &path, const void *contents, const size_t byteLength)
     {
         m_archive.writePtr(path, contents, byteLength);
-    }
-
-    template<typename iter, typename T>
-    void GTAR::writeIndividual(const string &path, const iter &start, const iter &end)
-    {
-        vector<T> buffer(start, end);
-
-        maybeSwapEndian<T>(&buffer[0], buffer.size()*sizeof(T));
-        writePtr(path, &buffer[0], buffer.size()*sizeof(T));
-    }
-
-    template<typename T>
-    void GTAR::writeUniform(const string &path, const T &val)
-    {
-        T local(val);
-
-        maybeSwapEndian<T>(&local, sizeof(T));
-        writePtr(path, &local, sizeof(T));
-    }
-
-    template<typename T>
-    SharedArray<T> GTAR::readIndividual(const string &path)
-    {
-        SharedArray<char> bytes(m_archive.read(path));
-
-        maybeSwapEndian<T>((T*) bytes.get(), bytes.size());
-        return *((T*) bytes.get());
-    }
-
-    template<typename T>
-    T GTAR::readUniform(const string &path)
-    {
-        SharedArray<char> bytes(m_archive.read(path));
-
-        maybeSwapEndian<T>((T*) bytes.get(), bytes.size());
-        return *((T*) bytes.get());
     }
 
     SharedArray<char> GTAR::readBytes(const string &path)
--- src/GTAR.hpp
+++ src/GTAR.hpp
@@ -10,6 +10,8 @@
 #ifndef __GTAR_HPP_
 #define __GTAR_HPP_
 
+#define IS_BIG_ENDIAN (*(uint16_t *)"\0\xff" < 0x100)
+
 namespace gtar{
 
     using std::string;
@@ -46,8 +48,62 @@ namespace gtar{
     private:
         Archive m_archive;
     };
-}
 
-#include "GTAR.cpp"
+    // Swap the bytes of a series of characters if this is a big-endian machine
+    template<typename T>
+    void maybeSwapEndian(T *target, size_t byteLength)
+    {
+        char *recast((char*) target);
+
+        if(byteLength % sizeof(T) != 0)
+            throw runtime_error("Trying to convert an incorrect number of bytes to little-endian");
+
+        if(IS_BIG_ENDIAN)
+        {
+            for(size_t i(0); i < byteLength/sizeof(T); ++i)
+            {
+                for(size_t j(0); j < sizeof(T)/2; ++j)
+                    swap(recast[i*sizeof(T) + j], recast[(i + 1)*sizeof(T) - j - 1]);
+            }
+        }
+    }
+
+    template<typename iter, typename T>
+    void GTAR::writeIndividual(const string &path, const iter &start, const iter &end)
+    {
+        vector<T> buffer(start, end);
+
+        maybeSwapEndian<T>(&buffer[0], buffer.size()*sizeof(T));
+        writePtr(path, &buffer[0], buffer.size()*sizeof(T));
+    }
+
+    template<typename T>
+    void GTAR::writeUniform(const string &path, const T &val)
+    {
+        T local(val);
+
+        maybeSwapEndian<T>(&local, sizeof(T));
+        writePtr(path, &local, sizeof(T));
+    }
+
+    template<typename T>
+    SharedArray<T> GTAR::readIndividual(const string &path)
+    {
+        SharedArray<char> bytes(m_archive.read(path));
+
+        maybeSwapEndian<T>((T*) bytes.get(), bytes.size());
+        return *((T*) bytes.get());
+    }
+
+    template<typename T>
+    T GTAR::readUniform(const string &path)
+    {
+        SharedArray<char> bytes(m_archive.read(path));
+
+        maybeSwapEndian<T>((T*) bytes.get(), bytes.size());
+        return *((T*) bytes.get());
+    }
+
+}
 
 #endif
