e9b978013771c1ddbdfee587828f5707e0b48827
Matthew Spellings <mspells@umich.edu>
2014-04-29 09:10:36 -0400

    Added option to disable CRC32 checks on reads with miniz

--- setup.py
+++ setup.py
@@ -1,10 +1,17 @@
 #!/usr/bin/env python
 
+import sys
 from distutils.core import Extension, setup
+
+macros = []
+
+if '--disable-read-check' in sys.argv:
+    macros.append(('MINIZ_DISABLE_ZIP_READER_CRC32_CHECKS', None))
+    sys.argv.remove('--disable-read-check')
 
 cFiles = ['Archive.cpp', 'capi.cpp', 'GTAR.cpp', 'vogl_miniz.cpp', 'vogl_miniz_zip.cpp']
 moduleSrc = ['src/{}'.format(name) for name in cFiles]
-cExt = Extension('gtar._libgetar', moduleSrc)
+cExt = Extension('gtar._libgetar', moduleSrc, define_macros=macros)
 
 setup(name='gtar',
       version='0.1',
