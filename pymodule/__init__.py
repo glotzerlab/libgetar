eca3795bda1c4d2f5cf712d9b4067bca793a8bd5
Matthew Spellings <mspells@umich.edu>
2014-04-30 16:58:44 -0400

    Intermediate commit, playing with cython

--- pymodule/__init__.py
+++
@@ -1,8 +0,0 @@
-
-from . import enums
-from . import libgetar
-from . import gtar
-
-from enums import *
-from libgetar import *
-from gtar import *
--- setup.py
+++ setup.py
@@ -1,17 +1,14 @@
 #!/usr/bin/env python
 
-import sys
+import os, sys
 from distutils.core import Extension, setup
+from Cython.Build import cythonize
 
 macros = []
 
 if '--disable-read-check' in sys.argv:
     macros.append(('MINIZ_DISABLE_ZIP_READER_CRC32_CHECKS', None))
     sys.argv.remove('--disable-read-check')
-
-cFiles = ['Archive.cpp', 'capi.cpp', 'GTAR.cpp', 'Record.cpp', 'vogl_miniz.cpp', 'vogl_miniz_zip.cpp']
-moduleSrc = ['src/{}'.format(name) for name in cFiles]
-cExt = Extension('gtar._libgetar', moduleSrc, define_macros=macros)
 
 setup(name='gtar',
       version='0.1',
@@ -20,6 +17,5 @@ setup(name='gtar',
       author_email='mspells@umich.edu',
       url='',
       packages=['gtar'],
-      package_dir={'gtar': 'pymodule'},
-      ext_modules=[cExt]
+      ext_modules=cythonize(os.path.join('gtar', '*.pyx'), include_dirs=['.'])
 )
