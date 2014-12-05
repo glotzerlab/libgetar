78ddeac1894c3ad9878133ed46440e7871791097
Matthew Spellings <mspells@umich.edu>
2014-12-05 17:36:56 -0500

    Bump to v0.3.1

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ else:
                          include_dirs=[numpy.get_include()])]
 
 setup(name='gtar',
-      version='0.3',
+      version='0.3.1',
       description='GTAR format file wrapper',
       author='Matthew Spellings',
       author_email='mspells@umich.edu',
