e24240b5e8240c25b208572439723250086dbaf9
Matthew Spellings <mspells@umich.edu>
2015-06-04 22:28:11 -0400

    Added gtar.cat

---
+++ gtar/cat.py
@@ -0,0 +1,77 @@
+"""
+Take records from multiple getar-formatted files and place them
+into an output file. In case of name conflicts, records from the last
+input file take precedence.
+
+::
+
+   usage: cat.py [-h] [-o OUTPUT] ...
+
+   Command-line archive copier or translator
+
+   positional arguments:
+     inputs                Input files to read
+
+   optional arguments:
+     -h, --help            show this help message and exit
+     -o OUTPUT, --output OUTPUT
+                           File to write to
+"""
+
+import argparse
+from collections import defaultdict
+import os
+
+import gtar
+
+parser = argparse.ArgumentParser(
+    description='Command-line archive copier or translator')
+parser.add_argument('inputs', nargs=argparse.REMAINDER,
+                    help='Input files to read')
+parser.add_argument('-o', '--output', required=True,
+                    help='File to write to')
+
+def main(inputs, output):
+    """Take all records from a set of getar-formatted files and output them to another.
+
+    :param inputs: Input filenames to concatenate
+    :param output: Output filename (can be the same as input)
+
+    """
+    nameHalves = os.path.splitext(output)
+    tempName = output
+
+    while os.path.exists(tempName):
+        nameHalves = (nameHalves[0] + '_', nameHalves[1])
+        tempName = nameHalves[0] + nameHalves[1]
+
+    recordFiles = {}
+    for input in inputs:
+        with gtar.GTAR(input, 'r') as inpFile:
+            recs = {rec: inpFile.queryFrames(rec) for rec in inpFile.getRecordTypes()}
+
+            for rec in recs:
+                frames = recs[rec]
+                for frame in frames:
+                    rec.setIndex(frame)
+                    recordFiles[rec.getPath()] = input
+
+    sourceRecords = defaultdict(list)
+    for path in recordFiles:
+        sourceRecords[recordFiles[path]].append(path)
+
+    try:
+        with gtar.GTAR(tempName, 'w') as out:
+            for source in sourceRecords:
+                allPaths = sorted(sourceRecords[source])
+                with gtar.GTAR(source, 'r') as inp:
+                    for path in allPaths:
+                        out.writePath(path, inp.readPath(path))
+
+        if not os.path.samefile(tempName, output):
+            os.rename(tempName, output)
+    finally:
+        if os.path.exists(tempName) and not os.path.samefile(tempName, output):
+            os.remove(tempName)
+
+if __name__ == '__main__': main(**vars(parser.parse_args()))
--- gtar/copy.py
+++ gtar/copy.py
@@ -3,7 +3,7 @@ Copy each record from one getar-formatted file to another.
 
 ::
 
-   usage: python -m gtar.copy [-h] [--ignore-empty] input output
+   usage: python -m gtar.copy [-h] input output
 
    Command-line archive copier or translator
 
@@ -13,13 +13,11 @@ Copy each record from one getar-formatted file to another.
 
    optional arguments:
      -h, --help      show this help message and exit
-     --ignore-empty  Ignore empty files
 """
 
 import argparse
-import os
 
-import gtar
+from . import cat
 
 parser = argparse.ArgumentParser(
     description='Command-line archive copier or translator')
@@ -27,49 +25,14 @@ parser.add_argument('input',
                     help='Input file to read')
 parser.add_argument('output',
                     help='File to write to')
-parser.add_argument('--ignore-empty', action='store_true', default=False,
-                    help='Ignore empty files')
 
-def main(input, output, ignore_empty=False):
+def main(input, output):
     """Copy each record from one getar-formatted file to another.
 
     :param input: Input filename
     :param output: Output filename (can be the same as input)
-    :param ignore_empty: if True, ignore empty records rather than copying them over
 
     """
-
-    nameHalves = os.path.splitext(output)
-    tempName = output
-
-    while os.path.exists(tempName):
-        nameHalves = (nameHalves[0] + '_', nameHalves[1])
-        tempName = nameHalves[0] + nameHalves[1]
-
-    try:
-        with gtar.GTAR(input, 'r') as inpFile, gtar.GTAR(tempName, 'w') as outFile:
-
-            recs = {rec: inpFile.queryFrames(rec) for rec in inpFile.getRecordTypes()}
-
-            for rec in recs:
-                frames = recs[rec]
-
-                for frame in frames:
-                    rec.setIndex(frame)
-
-                    data = inpFile.getRecord(rec, frame)
-                    try:
-                        nonempty = len(data)
-                    except TypeError:
-                        nonempty = data
-
-                    if nonempty or not ignore_empty:
-                        outFile.writeRecord(rec, data)
-
-        if not os.path.samefile(tempName, output):
-            os.rename(tempName, output)
-    finally:
-        if os.path.exists(tempName) and not os.path.samefile(tempName, output):
-            os.remove(tempName)
+    cat.main(inputs=[input], output=output)
 
 if __name__ == '__main__': main(**vars(parser.parse_args()))
