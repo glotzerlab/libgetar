32bd89a2464cca7e58859f37f1e5f76f893d27d5
Matthew Spellings <mspells@umich.edu>
2015-11-25 13:15:14 -0500

    Use RTD sphinx theme

--- doc/source/conf.py
+++ doc/source/conf.py
@@ -23,6 +23,9 @@ import os
 #sys.path.insert(0, os.path.abspath('.'))
 sys.path.insert(0, os.path.abspath('../../gtar'))
 
+# determine if we are on readthedocs.org
+on_rtd = os.environ.get('READTHEDOCS', None) == 'True'
+
 # -- General configuration ------------------------------------------------
 
 # If your documentation needs a minimal Sphinx version, state it here.
@@ -107,7 +110,9 @@ pygments_style = 'sphinx'
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+if not on_rtd:
+    html_theme = 'sphinx_rtd_theme'
+# Leave the theme default on RTD
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
